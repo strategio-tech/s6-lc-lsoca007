e5a17a7a6d1920d10f27020643507c9321b22198
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2022-12-09 15:52:46 -0500

    feat: solution

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -8,12 +8,20 @@ public class LoanCalculator {
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
      * 
-     * @param ???
-     * @return ???
+     * @param amount  loan amount
+     * @return Remaining loan amount
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+
+        // Calculate the remaining loan amount after 3 months
+        int remainingAmount = amount;
+        for (int i = 0; i < 3; i++) {
+            //remainingAmount *= (1 - 0.1);
+            remainingAmount = (int)(Math.ceil(remainingAmount * 0.9));
+        }
+
+        // Return the remaining loan amount
+        return remainingAmount;
     }
 
     public static void main(String[] args) {
